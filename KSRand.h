447990cad3213a01babb8318397981284d3ace85
ksoftroid <ksoft.android@gmail.com>
2013-04-04 21:22:36 +0900

    Add Box Muler Function

--- KSRand.h
+++ KSRand.h
@@ -30,4 +30,49 @@ namespace ksmath{
 		template<typename T> T get1(){ return rand()/static_cast<T>(RAND_MAX); }
 	};
 
+	//--------------------------------------------------------------------------/
+	// Class Name	/ CKSRandNorm
+	//--------------------------------------------------------------------------/
+	// Abstruct		/ 正規分布乱数
+	// History		/ 2013.04.02 Kei Hishinuma First Edition					
+	//--------------------------------------------------------------------------/
+	class CKSRandNorm
+	{
+	private:
+		double	mAvg;			// 平均
+		double	mNorm;			// sqrt(分散)=標準偏差
+		CKSRand	mRand;			// 乱数生成
+		bool	mbSin;			// ２つのうちどっちを取得するか
+		double	mAlpha;			// ボックスミューラの係数１
+		double	mBeta;			// ボックスミューラの係数２
+
+	public:
+		//--------------------------------------------------------------------------/
+		// Function		/ コンストラクタ
+		//--------------------------------------------------------------------------/
+		// Abstruct		/ 正規分布の平均値と分散を指定すること
+		// History		/ 2013.04.02 Kei Hishinuma First Edition					/
+		//--------------------------------------------------------------------------/
+		CKSRandNorm(double avg, double variance )
+			: mAvg(avg)
+			, mNorm(sqrt(variance))
+			, mbSin(true)
+			, mAlpha(0.0)
+			, mBeta(0.0)
+		{}
+		~CKSRandNorm(){}
+		void init(){mRand.init();}
+		void init(int seed){mRand.init(seed);}
+		double get(){
+			if(mbSin){
+				mbSin = !mbSin;
+				mAlpha = sqrt( -2.0 * log( mRand.get1<double>() ) );
+				mBeta = TPI2<double>() * mRand.get1<double>();
+				return mAvg + mNorm * sin( mBeta ) * mAlpha;
+			}
+			mbSin = !mbSin;
+			return mAvg + mNorm * cos( mBeta ) * mAlpha;
+		}
+	};
+
 }
